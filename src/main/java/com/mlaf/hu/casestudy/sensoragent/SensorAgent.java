d9c283a1c4d450efc266c71772876f562020a2b9
Auke Bakker <aacoba@aacoba.net>
2018-01-22 15:12:24 +0100

    Basic Boilerplate Code

---
+++ src/main/java/com/mlaf/hu/casestudy/sensoragent/SensorAgent.java
@@ -0,0 +1,13 @@
+package com.mlaf.hu.casestudy.sensoragent;
+
+public class SensorAgent extends com.mlaf.hu.sensoragent.SensorAgent{
+    @Override
+    protected String getInstructionXML() {
+        return null;
+    }
+
+    @Override
+    public void onReceivingRefuseRegistration() {
+
+    }
+}
